2e472c55c3b71842cb2388792692328653cb9517
Prateek Sharma <91019339+PrateekSharma007@users.noreply.github.com>
2023-08-17 17:54:24 +0530

    almost done

--- Jwt/generatekeys.js
+++ Jwt/generatekeys.js
@@ -2,4 +2,4 @@ const crypto = require('crypto')
 
 const key1 = crypto.randomBytes(32).toString('hex');
 const key2 = crypto.randomBytes(32).toString('hex');
-console.log({key1,key2}) ;
+console.log({key1,key2 }) ;
--- Jwt/jwt1.js
+++ Jwt/jwt1.js
@@ -5,20 +5,20 @@ const createError = require("http-errors")
 
 
 
-module.exports = {
-    signaccesstoken : (userId) => {
-        return new Promise((resolve,reject) => { 
-            const payload = {
-            }
-            const secret = "c304a44766afb7c1174f138c00e629b9a3e24dd06b4258b3f5f25e09818c5507"
-            const options = {
-                expiresIn : "1h",
+// module.exports = {
+//     signaccesstoken : (userId) => {
+//         return new Promise((resolve,reject) => { 
+//             const payload = {
+//             }
+//             const secret = "c304a44766afb7c1174f138c00e629b9a3e24dd06b4258b3f5f25e09818c5507"
+//             const options = {
+//                 expiresIn : "1h",
 
-            }
-            jwt.sign(payload,secret,options ,(err,token) =>{
-                if(err) reject(err)
-                resolve(token)
-            })
-        })
-    }
-}
+//             }
+//             jwt.sign(payload,secret,options ,(err,token) =>{
+//                 if(err) reject(err)
+//                 resolve(token)
+//             })
+//         })
+//     }
+// }
